390208e46d7c4b7d5072a6601c3a7a60b19ba595
rjuaum <joaoleet10@hotmail.com>
2025-06-05 12:19:55 +0100

    test

--- webserver.sh
+++ webserver.sh
@@ -15,23 +15,21 @@ sudo systemctl restart apache2
 echo "<?php phpinfo(); ?>" | sudo tee /var/www/html/info.php
 
 # Cria testdb.php com variáveis substituídas
-sudo tee /var/www/html/testdb.php > /dev/null <<EOF
-<?php
-\$servername = "$DB_HOST";
-\$database = "mysql";
-\$username = "$DB_USER";
-\$password = "$DB_PASS";
+echo "<?php
+\$servername = \"${DB_HOST}\";
+\$database = \"mysql\";
+\$username = \"${DB_USER}\";
+\$password = \"${DB_PASS}\";
 
 // Cria a conexão
 \$conn = new mysqli(\$servername, \$username, \$password, \$database);
 
 // Verifica a conexão
 if (\$conn->connect_error) {
-    die("Connection failed: " . \$conn->connect_error);
+    die(\"Connection failed: \" . \$conn->connect_error);
 }
-echo "Connected successfully";
-?>
-EOF
+echo \"Connected successfully\";
+?>" > /var/www/html/testdb.php
 
 # Define permissões apropriadas (opcional)
 sudo chmod 644 /var/www/html/info.php /var/www/html/testdb.php
