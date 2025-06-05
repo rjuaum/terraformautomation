413c24df89a4dbdacc24e58cee5c79c1853aae6f
rjuaum <joaoleet10@hotmail.com>
2025-06-05 11:12:15 +0100

    changed mysql query

--- mysql-server-setup.sh
+++ mysql-server-setup.sh
@@ -18,13 +18,9 @@ DB_USER="${DB_USER}"
 DB_PASS="${DB_PASS}"
 
 # Cria utilizador apenas se ainda não existir
-USER_EXISTS=$(mysql -u root -e "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user='$DB_USER')" | tail -n 1)
-if [ "$USER_EXISTS" = "0" ]; then
-  mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
-fi
-
-# Concede permissões totais ao utilizador
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+sudo mysql --execute "CREATE USER $DB_USER@'%' IDENTIFIED BY '$DB_PASSWORD';"
+sudo mysql --execute "GRANT ALL PRIVILEGES ON mysql.* TO $DB_USER@'%';"
+sudo mysql --execute "FLUSH PRIVILEGES;"
 
 # Reinicia o MySQL para aplicar alterações
 sudo systemctl restart mysql
